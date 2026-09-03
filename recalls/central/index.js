614c321415ea5e094f922b8a17c6a39596275e9b
ammaranes <ammar.bahamdeen@gmail.com>
2026-09-04 00:43:02 +0300

    Add files via upload

---
+++ recalls/central/index.js
@@ -0,0 +1,151 @@
+// ============================================================================
+// KETOFOLDOC QUESTION BANK - MASTER INDEX (central_1.js to central_52.js)
+// ============================================================================
+
+import * as c1 from './central_1.js';
+import * as c2 from './central_2.js';
+import * as c3 from './central_3.js';
+import * as c4 from './central_4.js';
+import * as c5 from './central_5.js';
+import * as c6 from './central_6.js';
+import * as c7 from './central_7.js';
+import * as c8 from './central_8.js';
+import * as c9 from './central_9.js';
+import * as c10 from './central_10.js';
+import * as c11 from './central_11.js';
+import * as c12 from './central_12.js';
+import * as c13 from './central_13.js';
+import * as c14 from './central_14.js';
+import * as c15 from './central_15.js';
+import * as c16 from './central_16.js';
+import * as c17 from './central_17.js';
+import * as c18 from './central_18.js';
+import * as c19 from './central_19.js';
+import * as c20 from './central_20.js';
+import * as c21 from './central_21.js';
+import * as c22 from './central_22.js';
+import * as c23 from './central_23.js';
+import * as c24 from './central_24.js';
+import * as c25 from './central_25.js';
+import * as c26 from './central_26.js';
+import * as c27 from './central_27.js';
+import * as c28 from './central_28.js';
+import * as c29 from './central_29.js';
+import * as c30 from './central_30.js';
+import * as c31 from './central_31.js';
+import * as c32 from './central_32.js';
+import * as c33 from './central_33.js';
+import * as c34 from './central_34.js';
+import * as c35 from './central_35.js';
+import * as c36 from './central_36.js';
+import * as c37 from './central_37.js';
+import * as c38 from './central_38.js';
+import * as c39 from './central_39.js';
+import * as c40 from './central_40.js';
+import * as c41 from './central_41.js';
+import * as c42 from './central_42.js';
+import * as c43 from './central_43.js';
+import * as c44 from './central_44.js';
+import * as c45 from './central_45.js';
+import * as c46 from './central_46.js';
+import * as c47 from './central_47.js';
+import * as c48 from './central_48.js';
+import * as c49 from './central_49.js';
+import * as c50 from './central_50.js';
+import * as c51 from './central_51.js';
+import * as c52 from './central_52.js';
+
+// Resolves chapter data regardless of export convention (central_n, chapter01, or default)
+const resolve = (mod, name) => mod[name] || mod.chapter01 || mod.default || Object.values(mod)[0];
+
+export const central_1 = resolve(c1, 'central_1');
+export const central_2 = resolve(c2, 'central_2');
+export const central_3 = resolve(c3, 'central_3');
+export const central_4 = resolve(c4, 'central_4');
+export const central_5 = resolve(c5, 'central_5');
+export const central_6 = resolve(c6, 'central_6');
+export const central_7 = resolve(c7, 'central_7');
+export const central_8 = resolve(c8, 'central_8');
+export const central_9 = resolve(c9, 'central_9');
+export const central_10 = resolve(c10, 'central_10');
+export const central_11 = resolve(c11, 'central_11');
+export const central_12 = resolve(c12, 'central_12');
+export const central_13 = resolve(c13, 'central_13');
+export const central_14 = resolve(c14, 'central_14');
+export const central_15 = resolve(c15, 'central_15');
+export const central_16 = resolve(c16, 'central_16');
+export const central_17 = resolve(c17, 'central_17');
+export const central_18 = resolve(c18, 'central_18');
+export const central_19 = resolve(c19, 'central_19');
+export const central_20 = resolve(c20, 'central_20');
+export const central_21 = resolve(c21, 'central_21');
+export const central_22 = resolve(c22, 'central_22');
+export const central_23 = resolve(c23, 'central_23');
+export const central_24 = resolve(c24, 'central_24');
+export const central_25 = resolve(c25, 'central_25');
+export const central_26 = resolve(c26, 'central_26');
+export const central_27 = resolve(c27, 'central_27');
+export const central_28 = resolve(c28, 'central_28');
+export const central_29 = resolve(c29, 'central_29');
+export const central_30 = resolve(c30, 'central_30');
+export const central_31 = resolve(c31, 'central_31');
+export const central_32 = resolve(c32, 'central_32');
+export const central_33 = resolve(c33, 'central_33');
+export const central_34 = resolve(c34, 'central_34');
+export const central_35 = resolve(c35, 'central_35');
+export const central_36 = resolve(c36, 'central_36');
+export const central_37 = resolve(c37, 'central_37');
+export const central_38 = resolve(c38, 'central_38');
+export const central_39 = resolve(c39, 'central_39');
+export const central_40 = resolve(c40, 'central_40');
+export const central_41 = resolve(c41, 'central_41');
+export const central_42 = resolve(c42, 'central_42');
+export const central_43 = resolve(c43, 'central_43');
+export const central_44 = resolve(c44, 'central_44');
+export const central_45 = resolve(c45, 'central_45');
+export const central_46 = resolve(c46, 'central_46');
+export const central_47 = resolve(c47, 'central_47');
+export const central_48 = resolve(c48, 'central_48');
+export const central_49 = resolve(c49, 'central_49');
+export const central_50 = resolve(c50, 'central_50');
+export const central_51 = resolve(c51, 'central_51');
+export const central_52 = resolve(c52, 'central_52');
+
+// Sequential registry of all 52 chunks
+export const allChapters = [
+  central_1, central_2, central_3, central_4, central_5,
+  central_6, central_7, central_8, central_9, central_10,
+  central_11, central_12, central_13, central_14, central_15,
+  central_16, central_17, central_18, central_19, central_20,
+  central_21, central_22, central_23, central_24, central_25,
+  central_26, central_27, central_28, central_29, central_30,
+  central_31, central_32, central_33, central_34, central_35,
+  central_36, central_37, central_38, central_39, central_40,
+  central_41, central_42, central_43, central_44, central_45,
+  central_46, central_47, central_48, central_49, central_50,
+  central_51, central_52
+];
+
+// Flattened array of all questions across all chunks
+export const allQuestions = allChapters.flatMap(ch => ch?.questions || []);
+
+// Platform utility helpers
+export const getChapterById = (id) => {
+  return allChapters.find(ch => ch?.id === Number(id)) || null;
+};
+
+export const getQuestionById = (id) => {
+  return allQuestions.find(q => q?.id === Number(id)) || null;
+};
+
+export const getTotalQuestionsCount = () => allQuestions.length;
+export const getTotalChaptersCount = () => allChapters.length;
+
+export default {
+  chapters: allChapters,
+  questions: allQuestions,
+  getChapterById,
+  getQuestionById,
+  getTotalQuestionsCount,
+  getTotalChaptersCount
+};
